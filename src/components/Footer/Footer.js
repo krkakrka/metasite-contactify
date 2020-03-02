5080b7d484285169ee67612f8df6c1805cc8426f
Karolis Lesevicius <karolis.lesevicius@gmail.com>
2020-03-02 11:47:55 +0200

    Layout <Footer>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,15 @@ import './Footer.css';
 function Footer() {
   return (
     <div className="Footer">
-      Footer
+      <div className="Footer-left">
+        Links (same as header) and copyright
+      </div>
+      <div className="Footer-middle">
+        Sync and support
+      </div>
+      <div className="Footer-right">
+        Links (same as header dropdown)
+      </div>
     </div>
   );
 }
