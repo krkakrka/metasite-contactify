e720cc6bb69c0a7cf8c68112ca83c6cac9053f80
Karolis Lesevicius <karolis.lesevicius@gmail.com>
2020-03-03 14:54:42 +0200

    Style header links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Header.css';
 
-function Header() {
+function Header({ mainLinks, dropdownLinks }) {
   return (
     <div className="Header">
       <img className="Header-logo" />
-      <div className="Header-links">Links</div>
+      <div className="Header-links">
+        {mainLinks.map((link, index) => (
+          <a className={`link ${index === 1 ? 'active' : ''}`} href={link.href}>{link.label}</a>
+        ))}
+      </div>
       <input className="Header-search" type="text" placeholder="search" />
       <button className="Header-menu-button">Username</button>
     </div>
